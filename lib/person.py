12e3c7debc4d525a732fbf5fd11e75d09b2f3c2e
Shelli Silverstein <robot.shelli@gmail.com>
2024-07-31 15:10:50 -0400

    completed lab

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,28 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Shelli", job="Sales"):
+        self.name = name
+        self.job = job
+    
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = job
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
+
